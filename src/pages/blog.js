705c296724eba1257bf8146b28240251dff33b74
Sean Parkin <sparkin@gmail.com>
2019-03-14 14:52:41 -0500

    Created index of blogs

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,12 +2,35 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { graphql } from "gatsby";
 
-const BlogPage = () => (
+const BlogPage = ({data}) => (
     <Layout>
         <SEO title="Blog" keywords={[`gatsby`,`application`,`react`]}/>
-        <h1>This is the blog page!!</h1>
+        <h3>Index</h3>
+        <ul>
+        {data.allMarkdownRemark.edges.map(index => (
+            <li>
+            <a href={index.node.frontmatter.path}>{index.node.frontmatter.title}</a>
+            </li>
+        ))}
+        </ul>
     </Layout>
 )
+
+export const pageQuery= graphql`
+    query IndexQuery {
+        allMarkdownRemark(limit: 10) {
+            edges {
+              node {
+                frontmatter {
+                  path
+                  title
+                }
+              }
+            }
+        }
+    }
+`
 
 export default BlogPage
---
+++ src/posts/3-14-19/index.md
@@ -0,0 +1,5 @@
+---
+path: '/second-post'
+title: 'Second Blog Post'
+---
+This is the 2nd post that will eventually get deleted!!
