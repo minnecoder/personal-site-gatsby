54ad8b5843c372fb8456a58bbd2a46381f6da007
Sean <sdparkin@gmail.com>
2019-04-29 10:01:02 -0500

    Redesign of Portfolio Section

--- src/components/work.js
+++ src/components/work.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import { StaticQuery, graphql } from 'gatsby'
+import PortfolioItem from './PortfolioItem'
 import media from './media'
 
 const Work = () => (
@@ -50,23 +51,7 @@ const Work = () => (
               {portfolioData.map(({ node }) => {
                 const image = portfolioImages.find(n => n.node.relativePath === `projects/${node.img}`)
                 const imgSrc = image.node.childImageSharp.fluid.src
-                return (
-                  <div className="PortfolioItem">
-                    {/* <Img title={} alt=" Image of project" sizes={imageSizes} /> */}
-                    <div className="Overlay">
-                      <h3>{node.name}</h3>
-                      <p>{node.description}</p>
-                      <p>
-                        <strong>Technologies Used:</strong>
-                        {node.tech}
-                      </p>
-                      <WorkBtns>
-                        <WorkBtn href={node.website}>Website</WorkBtn>
-                        <WorkBtn href={node.github}>GitHub</WorkBtn>
-                      </WorkBtns>
-                    </div>
-                  </div>
-                )
+                return <PortfolioItem node={node} imgSrc={imgSrc} />
               })}
             </PortfolioList>
           </Portfolio>
@@ -112,64 +97,6 @@ const WorkTypes = styled.div`
 
 const PortfolioList = styled.div`
   display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  justify-content: start;
-`
-
-// const Overlay = styled.div`
-//   position: absolute;
-//   top: 0;
-//   bottom: 0;
-//   left: 0;
-//   right: 0;
-//   height: 100%;
-//   width: 100%;
-//   opacity: 0;
-//   transition: 0.5s ease;
-//   background-color: #fff;
-//   display: flex;
-//   flex-direction: column;
-//   justify-content: space-between;
-//   align-items: center;
-//   > h3 {
-//     font-size: 20px;
-//     padding-top: 20px;
-//   }
-//   p {
-//   }
-// `
-
-// const PortfolioItem = styled.div`
-//   position: relative;
-//   width: 33.33%;
-//   ${media.desktop`
-//   width: 50%;
-//   `}
-//   ${media.plusphone`
-//   width: 100%;
-//   `}
-//   > img {
-//     display: block;
-//     width: 100%;
-//     height: auto;
-//   }
-//   &:hover ${Overlay} {
-//     opacity: 1;
-//   }
-// `
-
-const WorkBtns = styled.div`
-  order: 2;
-  padding-bottom: 30px;
-`
-
-const WorkBtn = styled.a`
-  margin: 0 20px;
-  background: #fff;
-  border: solid 1px #08fdd860;
-  border-radius: 4px;
-  padding: 6px 25px;
-  color: #38384f;
-  text-decoration: none;
+  flex-direction: column;
+  align-items: center;
 `
